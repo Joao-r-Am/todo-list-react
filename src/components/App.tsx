d82fb4993cfba930714d75a33d744a72c97557b2
Joao-r-Am <joaoric.amorim@gmail.com>
2023-07-17 13:18:53 -0300

    first commit todo-list

---
+++ src/components/App.tsx
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+import Navbar from "./Navbar";
+import TodoList from "./TodoList";
+import TodoContext from "../contexts/TodoContext";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddTodo from "./AddTodo";
+
+const App = () => {
+  return (
+    <TodoContext>
+      <Router>
+        <Navbar></Navbar>
+        <br />
+        <div className="uk-container">
+          <Routes>
+            <Route path="/create" element={<AddTodo />} />
+            <Route path="/" element={<TodoList />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </TodoContext>
+  );
+};
+
+export default App;
